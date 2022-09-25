c38dba366f421e6bc2793b2c52b828c105b9ca80
Vladimir Takhmazyan <takhmazyanvladimir@gmail.com>
2022-09-25 21:07:26 +0300

    add scripts for testapp

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get install -y mongodb-org
