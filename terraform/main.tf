3d76de9736d3700758ec19bb05dd2248f5d814f6
Vladimir Takhmazyan <takhmazyanvladimir@gmail.com>
2022-10-16 05:11:11 +0300

    add terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,42 +8,44 @@ terraform {
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
-  cloud_id = var.cloud_id
-  folder_id = var.folder_id
-  zone = var.zone
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
-name = "reddit-app"
-resources {
-  cores = 2
-  memory = 2
-}
-boot_disk {
-  initialize_params {
-    image_id = var.image_id
+  name  = "reddit-app${count.index}"
+  count = 2
+  zone  = var.zone
+  resources {
+    cores  = 2
+    memory = 2
   }
-}
-metadata = {
-  ssh-keys = "azathoth:${file(var.public_key_path)}"
-}
-network_interface {
-  subnet_id = var.subnet_id
-  nat = true
-}
-connection {
-type = "ssh"
-host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-user = "ubuntu"
-agent = false
-private_key = file(var.private_key_path)
-}
-provisioner "file" {
-  source = "files/puma.service"
-  destination = "/tmp/puma.service"
-}
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+  metadata = {
+    ssh-keys = "azathoth:${file(var.public_key_path)}"
+  }
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+  connection {
+    type        = "ssh"
+    host        = self.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
+    private_key = file(var.private_key_path)
+  }
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
 
-provisioner "remote-exec" {
-script = "files/deploy.sh"
-}
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,28 @@
-variable cloud_id{
+variable "cloud_id" {
   description = "Cloud"
 }
-variable folder_id {
+variable "folder_id" {
   description = "Folder"
 }
-variable zone {
+variable "zone" {
   description = "Zone"
   # Значение по умолчанию
   default = "ru-central1-a"
 }
-variable public_key_path {
+variable "public_key_path" {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable private_key_path {
+variable "private_key_path" {
   # Описание переменной
   description = "Path to the private key used for provisioner"
 }
-variable image_id {
+variable "image_id" {
   description = "Disk image"
 }
-variable subnet_id{
+variable "subnet_id" {
   description = "Subnet"
 }
-variable service_account_key_file{
+variable "service_account_key_file" {
   description = "key .json"
 }
