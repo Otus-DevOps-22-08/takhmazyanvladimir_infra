f789bb671fc966eff49ae552a56c04ed7f4aa973
Vladimir Takhmazyan <takhmazyanvladimir@gmail.com>
2022-10-27 14:36:25 +0300

    Terraform modules

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sleep 120
+sleep 20
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sleep 20
 apt update
+sleep 20
 apt-get install -y mongodb-org
+sleep 20
 systemctl start mongod
 systemctl enable mongod
