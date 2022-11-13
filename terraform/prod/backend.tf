446d0135a26f9c2e2944f719ca7da09c6c83e936
Vladimir Takhmazyan <takhmazyanvladimir@gmail.com>
2022-11-13 19:35:10 +0300

    Add application

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -5,7 +5,6 @@ terraform {
     region     = "ru-central1"
     key        = "terraform_stage.tfstate"
     access_key = "YCAJEhJKFIVuAHtPJGkCo5PFx"
-    secret_key = "YCPwYmxzVn1geg9_xv2Jvcoi65rCY0k9FVfnzfgz"
 
     skip_region_validation      = true
     skip_credentials_validation = true
